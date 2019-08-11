f1da4a7107f737cde6bd219fde77db26c84a0d97
Doug Devops <doug.macgregor@gmail.com>
2019-08-11 13:38:07 -0500

    End of section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DatingApp.API.Controllers
 {
   //POST http://localhost:5000/api/values/5
+  [Authorize]
   [Route("api/[controller]")]
   [ApiController]
   public class ValuesController : ControllerBase
@@ -29,6 +31,7 @@ namespace DatingApp.API.Controllers
     }
 
     // GET api/values/5
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetValue(int id)
     {
