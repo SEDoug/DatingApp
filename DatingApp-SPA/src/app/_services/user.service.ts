b2675e0b3d3346f75cb9430a9fe8e493de7eab3c
Doug Devops <doug.macgregor@gmail.com>
2019-09-15 12:14:42 -0500

    End of Section 10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -16,8 +16,12 @@ getUsers(): Observable<User[]> {
   return this.http.get<User[]>(this.baseUrl + 'users');
 }
 
-getUser(id): Observable<User> {
+getUser(id: string): Observable<User> {
   return this.http.get<User>(this.baseUrl + 'users/' + id);
+}
+
+updateUser(id: number, user: User) {
+  return this.http.put(this.baseUrl + 'users/' + id, user);
 }
 
 }
